4835a9ef0a8dc0f37c475993ba603bc7f4fbe0a8
TRSI <mrsv1820@gmail.com>
2026-04-22 01:28:20 +0300

    تحديث نشر الويب و render.yaml

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "63cae3d701a310ab0dab13582879e64c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5020b625e31456e170cd22766ca48594",
+"flutter_bootstrap.js": "66cb238fcce04f17799efe23342cbae8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
